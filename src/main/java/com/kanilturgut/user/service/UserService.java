b6c98eacb1850058f453a6e07283ae88e90b58e4
Kadir Anıl Turğut <kanilturgut@gmail.com>
2020-12-19 00:43:39 +0300

    eureka-client

--- src/main/java/com/kanilturgut/user/service/UserService.java
+++ src/main/java/com/kanilturgut/user/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
         User user = userRepository.findByUserId(userId);
         vo.setUser(user);
 
-        Department department = restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId(), Department.class);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(), Department.class);
         vo.setDepartment(department);
 
         return vo;
